658c9f2d79b5661e57a1dbdb9368b37409ab6607
Mohit Yadav <mohit.yadav@xebia.com>
2019-04-19 19:10:40 +0400

    update changes for script for osx

---
+++ scripts/setup-osx.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+echo "<<--------- Installing and unpacking gecko driver ------->>"
+curl -L https://github.com/mozilla/geckodriver/releases/download/v0.21.0/geckodriver-v0.21.0-macos.tar.gz | tar xz
+echo "<<--------- Installing and unpacking gecko driver Done ------->>"
