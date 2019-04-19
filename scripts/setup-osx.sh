57be5041b35f5473a168683877c5598d43e36e5f
Mohit Yadav <mohit.yadav@xebia.com>
2019-04-19 19:26:31 +0400

    add test setup files

--- scripts/setup-osx.sh
+++ scripts/setup-osx.sh
@@ -4,3 +4,7 @@
 echo "<<--------- Installing and unpacking gecko driver ------->>"
 curl -L https://github.com/mozilla/geckodriver/releases/download/v0.21.0/geckodriver-v0.21.0-macos.tar.gz | tar xz
 echo "<<--------- Installing and unpacking gecko driver Done ------->>"
+
+echo "<<----- Installing webdriver io ------>>"
+yarn add webdriverio
+echo "<<------ Webdriver io installed ------>>"
